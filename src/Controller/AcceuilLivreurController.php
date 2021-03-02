54ecb6e9ee0369d017d937c0c02cdb07c7f0fd1c
Vincent Brocheton <vinc.brocheton@gmail.com>
2021-03-02 15:30:49 +0100

    Livreur Page

--- src/Controller/AcceuilLivreurController.php
+++ src/Controller/AcceuilLivreurController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Livraison;
+use App\Entity\Livreur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,33 @@ class AcceuilLivreurController extends AbstractController
      */
     public function index(): Response
     {
+        $user = $this->getUser()->getLivreur()->getId();
+        $repo = $this->getDoctrine()->getRepository(Livraison::class);
+        $livraison = $repo->findBy(['livreur'=>$user, 'status'=>'Prise en charge livreur']);
+
         return $this->render('acceuil_livreur/index.html.twig', [
             'controller_name' => 'AcceuilLivreurController',
+            'livraisons' => $livraison
+        ]);
+    }
+
+    /**
+     * @Route("/livreur/validation/{id}", name="validation_commande")
+     */
+    public function validateComand(int $id): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Livraison::class);
+        $livraison = $repository->find($id);
+        $livraison->setStatus('Livré');
+        $livraison->setDateLivraison(new \DateTime());
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($livraison);
+        $entityManager->flush();
+
+        return $this->render('acceuil_livreur/validation.html.twig', [
+            'controller_name' => 'AcceuilLivreurController',
+            'livraison' => $livraison
         ]);
     }
 }
