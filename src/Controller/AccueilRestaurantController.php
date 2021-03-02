3a79871f88e28384ae4c6956bfdfea58896ae22e
CharlotteD <charlotte.druvent@gmail.com>
2021-03-02 09:53:12 +0100

    Create Restaurant controller login boostrap

---
+++ src/Controller/AccueilRestaurantController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilRestaurantController extends AbstractController
+{
+    /**
+     * @Route("/accueil/restaurant", name="accueil_restaurant")
+     */
+    public function index(): Response
+    {
+        return $this->render('accueil_restaurant/index.html.twig', [
+            'controller_name' => 'AccueilRestaurantController',
+        ]);
+    }
+}
