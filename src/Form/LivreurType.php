78fffffc4e2e70b9d8eff6a95176fd294068c12e
Vincent Brocheton <vinc.brocheton@gmail.com>
2021-03-04 16:21:19 +0100

    livreur page edit

--- src/Form/LivreurType.php
+++ src/Form/LivreurType.php
@@ -6,6 +6,7 @@ use App\Entity\Livreur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use function Sodium\add;
@@ -15,7 +16,11 @@ class LivreurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('typeVehicule')
+            ->add('typeVehicule', TextType::class, [
+                'attr' =>[
+                    'placeholder'=>'Avec quoi roulez-vous?'
+                ]
+            ])
             ->add('description', TextareaType::class, [
                 'attr' =>[
                     'placeholder'=>'Décrivez-vous.'
