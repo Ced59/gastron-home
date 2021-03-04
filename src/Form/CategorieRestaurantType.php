306d6cbb2e9b81789c589cfa7ff1bcde6d3feb47
Vincent Brocheton <vinc.brocheton@gmail.com>
2021-03-04 16:32:52 +0100

    edit some page

--- src/Form/CategorieRestaurantType.php
+++ src/Form/CategorieRestaurantType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\CategorieRestaurant;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,12 @@ class CategorieRestaurantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libelle')
+            ->add('libelle', TextType::class,[
+                'label' => 'Nom de la catégorie de restaurant',
+                'attr'=>[
+                    'placeholder'=>'Catégorie de restaurant'
+                ]
+            ])
         ;
     }
 
